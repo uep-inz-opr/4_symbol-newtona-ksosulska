1b9c64031874df4bd6976282adcffeaa70d17f2c
Kasia Sosulska <69992@student.ue.poznan.pl>
2022-04-23 00:43:55 +0200

    troszke inaczej

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import math
+
+
+def new(n,k):
+	if k==0 or k==n:
+		return 1
+	else:
+		return new(n-1,k)+new(n-1,k-1)
+
 n=int(input())
 k=int(input())
-print (math.comb(n,k))
+
+print (int(new(n,k)))
