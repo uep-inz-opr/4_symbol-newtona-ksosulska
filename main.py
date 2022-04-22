84263382ca301161a10eb7c39f3017dc5b79a611
Kasia Sosulska <69992@student.ue.poznan.pl>
2022-04-23 00:26:16 +0200

    math

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
 import math
-n = input("n= ")
-k = input("k= ")
-if n == k:
-    print(1)
-elif k == 1:
-    print(n)
-elif k > n:          
-    print(0)
-else:                
-    a = math.factorial(n)
-    b = math.factorial(k)
-    c = math.factorial(n-k)  
-    div = a // (b * c)
-    print(div)  
+n=int(input())
+k=int(input())
+print (math.comb(n,k))
