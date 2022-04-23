ba920d71d39f3da225bfdc2ebe05d8e2cfa1d07b
Kasia Sosulska <69992@student.ue.poznan.pl>
2022-04-23 11:17:16 +0200

    czytanie ze zrozumieniem

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+import math
 
+n, k = map(int, input().split())
 
-def new(n,k):
-	if k==0 or k==n:
-		return 1
-	else:
-		return new(n-1,k)+new(n-1,k-1)
-
-n=int(input())
-k=int(input())
-
-print (int(new(n,k)))
+print (math.comb(n,k))
